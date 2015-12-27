7973ccb7230ad2db11bd3ce973bdcfe8573455bd
Kevin Fitzpatrick <kevin@influxdb.com>
2015-12-27 10:39:39 -0800

    Cache the docker image after every build.
    Due to the large base image docker image, we're saving the produced
    docker image to ~/docker/image.tar, which is cached, and loading it
    before building. This avoids having to download it from outside
    CircleCI's network.
    
    For more information check out https://circleci.com/docs/docker
    
    An in depth use case can be found at
    http://tschottdorf.github.io/cockroach-docker-circleci-continuous-integration/

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 set -e
-docker run -i test-box ls /foo/test.txt > result.test.txt
+docker run -i testbox ls /foo/test.txt > result.test.txt
 echo "==> Validating the test results..."
 sh -c 'grep "foo/test.txt" result.test.txt'
