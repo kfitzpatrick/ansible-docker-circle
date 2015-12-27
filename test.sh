2febca8b2c979c245133b305ed908fed25445f2b
Kevin Fitzpatrick <kevin@influxdb.com>
2015-12-27 13:21:33 -0800

    Another small change to test caching

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 set -e
-docker run -i testbox ls /foo/testfile.txt > result.test.txt
+docker run -i testbox ls /foo/file.txt > result.test.txt
 echo "==> Validating the test results..."
-sh -c 'grep "foo/testfile.txt" result.test.txt'
+sh -c 'grep "foo/file.txt" result.test.txt'
